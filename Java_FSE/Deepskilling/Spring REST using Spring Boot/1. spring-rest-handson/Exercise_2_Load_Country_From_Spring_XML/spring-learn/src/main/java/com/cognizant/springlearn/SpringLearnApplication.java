a97381b5a89442ea235c02d4f34d60717863bc2b
Raghav <raghavagrawal0505@gmail.com>
2026-07-20 12:55:40 +0530

    Added Spring REST Handson 1 - Exercise 2: Load Country from Spring XML Configuration

---
+++ Java_FSE/Deepskilling/Spring REST using Spring Boot/1. spring-rest-handson/Exercise_2_Load_Country_From_Spring_XML/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,43 @@
+package com.cognizant.springlearn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+
+        LOGGER.info("Starting Spring Learn Application");
+
+        SpringApplication.run(SpringLearnApplication.class, args);
+
+        displayCountry();
+
+        LOGGER.info("Spring Learn Application Started Successfully");
+    }
+
+    public static void displayCountry() {
+
+        LOGGER.debug("START");
+
+        try (ClassPathXmlApplicationContext context =
+                     new ClassPathXmlApplicationContext("country.xml")) {
+
+            Country country = context.getBean("country", Country.class);
+
+            LOGGER.debug("Country : {}", country);
+            LOGGER.debug("Country Code : {}", country.getCode());
+            LOGGER.debug("Country Name : {}", country.getName());
+        }
+
+        LOGGER.debug("END");
+    }
+}
