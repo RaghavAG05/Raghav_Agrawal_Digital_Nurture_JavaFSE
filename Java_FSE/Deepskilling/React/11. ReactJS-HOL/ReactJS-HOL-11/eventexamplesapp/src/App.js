d24de70d190c02ce0a703d30edbba6bdd05aa692
Raghav <raghavagrawal0505@gmail.com>
2026-07-22 20:51:20 +0530

    Added ReactJS-HOL 11 event handling and currency converter

---
+++ Java_FSE/Deepskilling/React/11. ReactJS-HOL/ReactJS-HOL-11/eventexamplesapp/src/App.js
@@ -0,0 +1,22 @@
+import Counter from "./Counter";
+import CurrencyConverter from "./CurrencyConverter";
+
+function App() {
+
+    return (
+
+        <div>
+
+            <Counter />
+
+            <hr />
+
+            <CurrencyConverter />
+
+        </div>
+
+    );
+
+}
+
+export default App;
